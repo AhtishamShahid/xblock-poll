db5f407275c1a1a3bccd958102e2a7e10665cae3
Ahtisham Shahid <ahtisham300@gmail.com>
2020-04-10 16:41:11 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ def package_data(pkg, roots):
 
 setup(
     name='xblock-poll',
-    version='1.9.5',
+    version='1.9.88',
     description='An XBlock for polling users.',
     packages=[
         'poll',
